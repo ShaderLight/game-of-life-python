c96b4e04bb6f99c789e696033bada6ef0ad2c64e
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2022-05-27 13:52:22 +0200

    Update test_headless.py

--- test_headless.py
+++ test_headless.py
@@ -1,6 +1,12 @@
 import logging
+import os
+from time import sleep
 
 from modules import board
+
+
+# https://stackoverflow.com/a/684344
+cls = lambda: os.system('cls' if os.name=='nt' else 'clear')
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
 
@@ -11,9 +17,24 @@ board_data.toggle_cell_at(5, 5)
 board_data.toggle_cell_at(5, 6)
 board_data.toggle_cell_at(5, 7)
 
+board_data.toggle_cell_at(15, 5)
+board_data.toggle_cell_at(15, 6)
+board_data.toggle_cell_at(15, 7)
+board_data.toggle_cell_at(16, 6)
+board_data.toggle_cell_at(16, 5)
+board_data.toggle_cell_at(16, 4)
+
 print(board_data)
 
-for i in range(3):
+# Simple oscillators
+# https://upload.wikimedia.org/wikipedia/commons/c/c2/2-3_O1.gif
+# and
+# https://upload.wikimedia.org/wikipedia/commons/a/ac/2-3_unruhe.gif
+
+for i in range(10):
+    cls()
+    print("Oscillators")
     board_data.calculate_next_state_all()
     board_data.switch_to_next_state_all()
     print(board_data)
+    sleep(0.5)
