106cc705a6fc54fed28e021dd9316e265ef9e300
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2022-05-27 12:47:14 +0200

    Board - assigning neighbouring cells

--- modules/board.py
+++ modules/board.py
@@ -1,3 +1,4 @@
+from turtle import width
 from cell import Cell
 
 class Board:
@@ -10,7 +11,23 @@ class Board:
     
     def generate_cells(self):
         for y in range(self.height):
-            temporary_column = []
+            temporary_row = []
             for x in range(self.width):
-                temporary_column.append(Cell(x,y))
-            self.cells.append(temporary_column.copy())
+                temporary_row.append(Cell(x,y))
+            self.cells.append(temporary_row.copy())
+
+    def assign_neighbours(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                for pair in self.get_neighbouring_coords(x, y):
+                    self.cells[y][x] = self.cells[pair[1]][pair[0]]
+
+    def get_neighbouring_coords(self, x: int, y: int) -> list[tuple[int]]:
+        possible_coords = [(x, y+1), (x+1, y+1), (x+1, y), (x+1, y-1), (x, y-1), (x-1, y-1), (x-1, y), (x-1, y+1)]
+        actual_coords = []
+
+        for pair in possible_coords:
+            if pair[0] < self.width and pair[1] < self.height:
+                actual_coords.append(pair)
+
+        return actual_coords.copy()
